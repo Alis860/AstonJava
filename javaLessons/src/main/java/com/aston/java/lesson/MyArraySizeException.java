c8a63660e71f8b89d48c673e790929c25c3d69b7
Alis860 <olesyazub588@gmail.com>
2024-02-05 20:50:20 +0300

    My seventh homework

--- javaLessons/src/main/java/com/aston/java/lesson/MyArraySizeException.java
+++ javaLessons/src/main/java/com/aston/java/lesson/MyArraySizeException.java
@@ -2,6 +2,6 @@ package com.aston.java.lesson;
 
 public class MyArraySizeException extends Exception {
     public MyArraySizeException(String message) {
-        super(message);
+         super(message);
     }
 }
