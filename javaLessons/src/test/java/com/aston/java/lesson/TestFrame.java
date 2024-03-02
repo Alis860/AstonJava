3662dfb57638a2d4ce59be2ebc68586dad22ff2f
Alis860 <olesyazub588@gmail.com>
2024-03-02 17:05:54 +0300

    fix Lesson 14

--- javaLessons/src/test/java/com/aston/java/lesson/TestFrame.java
+++ javaLessons/src/test/java/com/aston/java/lesson/TestFrame.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestFrame extends WebDriverSettings {
 
@@ -44,30 +46,97 @@ public class TestFrame extends WebDriverSettings {
         WebElement continueButton = driver.findElement(By.xpath("//button[contains(text(), 'Продолжить')]"));
         continueButton.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        String[] windowHandles = driver.getWindowHandles().toArray(new String[0]);
 
-        driver.switchTo().window(windowHandles[0]);
-        By xpathSelector = By.xpath("(//app-payment-container//span)[1]");
-        wait.until(ExpectedConditions.presenceOfElementLocated(xpathSelector));
-        WebElement element = driver.findElement(xpathSelector);
-        element.sendKeys("Тестовые данные" + inputSum);
 
-        WebElement cardNumberFiled = driver.findElement(By.xpath("//label[text()='Номер карты']"));
-        String cardNumber = cardNumberFiled.getAttribute("placeholder");
-        System.out.println("Надпись в незаполненном поле 'Номер карты' нового окна: " + cardNumberFiled);
 
-        WebElement validityFiled = driver.findElement(By.xpath("//input[@type='tel']"));
-        String validity = validityFiled.getAttribute("placeholder");
-        System.out.println("Надпись в незаполненном поле 'Срок действия' нового окна: " + validity);
 
-        WebElement cvcFiled = driver.findElement(By.xpath("//input[@name='verification_value']"));
-        String cvc = cvcFiled.getAttribute("placeholder");
-        System.out.println("Надпись в незаполненном поле 'CVC' нового окна: " + cvc);
 
-        WebElement nameHolderFiled = driver.findElement(By.xpath("//input[@autocomplete='cc-name']"));
-        String nameHolder = nameHolderFiled.getAttribute("placeholder");
-        System.out.println("Надпись в незаполненном поле 'Имя держателя (как на карте)' нового окна: " + nameHolder);
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@app-payment-container/app-header]")));
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//            By xpathSelector = By.xpath("//app-payment-container/app-header//div//span[1]");
+//            wait.until(ExpectedConditions.presenceOfElementLocated(xpathSelector));
+//            WebElement element = driver.findElement(xpathSelector);
+//        WebElement frame = null;
+//        frame.sendKeys("Тестовые данные" + inputSum);
+
+
+//        List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+//
+//// Переключение на нужный фрейм (в данном случае, первый найденный фрейм)
+//        if (!frames.isEmpty()) {
+//            driver.switchTo().frame(frames.get(0)); // Переключение на фрейм с помощью индекса
+//            WebElement frameElement = driver.findElement(By.xpath("//app-payment-container/app-header//div//span[1]")); // Пример поиска элемента внутри фрейма
+//            frameElement.sendKeys("Тестовые данные"); // Пример взаимодействия с элементом внутри фрейма
+//
+//        } else {
+//            System.out.println("Фреймы не найдены");
+//        }
+
+
+
+//        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+//
+//        int frameIndex = -1;
+//        for (int i = 0; i < windowHandles.size(); i++) {
+//            driver.switchTo().window(windowHandles.get(i));
+//            if (driver.findElements(By.tagName("iframe")).size() > 0) {
+//                frameIndex = i;
+//                break;
+//            }
+//        }
+//
+//        if (frameIndex != -1) {
+//            List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+//            driver.switchTo().frame(frames.get(frameIndex)); // Переключаемся на фрейм с помощью элемента <iframe>
+//            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameIndex));
+//            // Проверяем, что мы переключены на фрейм
+//            WebElement frame = driver.findElement(By.xpath("//app-payment-container"));
+//            wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//            By xpathSelector = By.xpath("//app-payment-container/app-header//div//span[1]");
+//            wait.until(ExpectedConditions.presenceOfElementLocated(xpathSelector));
+//            WebElement element = driver.findElement(xpathSelector);
+//            frame.sendKeys("Тестовые данные" + inputSum);
+//        } else {
+//            System.out.println("Фрейм не найден");
+//        }
+
+
+
+
+        //app-payment-container/app-header//div//span[1]
+//        driver.switchTo().frame(0);
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//app-payment-container/app-header//div/span")));
+//
+//        WebElement frame = driver.findElement(By.xpath("//app-payment-container/app-header//div/span"));
+//        frame.sendKeys("Тестовые данные" + inputSum);
+
+
+
+
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(), 'BYN')]"))); // Используйте правильный XPath для поиска элемента во фрейме
+//        WebElement element = frame.findElement(By.xpath("//span[contains(text(), 'BYN')]")); // Ищите элемент в найденном фрейме
+//        element.sendKeys("Тестовые данные" + inputSum);
+
+//        WebElement cardNumberFiled = driver.findElement(By.xpath("//label[text()='Номер карты']"));
+//        String cardNumber = cardNumberFiled.getAttribute("placeholder");
+//        System.out.println("Надпись в незаполненном поле 'Номер карты' нового окна: " + cardNumberFiled);
+//
+//        WebElement validityFiled = driver.findElement(By.xpath("//input[@type='tel']"));
+//        String validity = validityFiled.getAttribute("placeholder");
+//        System.out.println("Надпись в незаполненном поле 'Срок действия' нового окна: " + validity);
+//
+//        WebElement cvcFiled = driver.findElement(By.xpath("//input[@name='verification_value']"));
+//        String cvc = cvcFiled.getAttribute("placeholder");
+//        System.out.println("Надпись в незаполненном поле 'CVC' нового окна: " + cvc);
+//
+//        WebElement nameHolderFiled = driver.findElement(By.xpath("//input[@autocomplete='cc-name']"));
+//        String nameHolder = nameHolderFiled.getAttribute("placeholder");
+//        System.out.println("Надпись в незаполненном поле 'Имя держателя (как на карте)' нового окна: " + nameHolder);
     }
 }
 //div[@class='app-wrapper__content']
